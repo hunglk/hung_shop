c1484101f6c42eefffb8b18bb1ce6c92cd7abdb7
hunglk <hunglk@smartosc.com>
2013-12-25 10:29:02 +0700

    sql

--- sql/shop25-12.sql
+++ sql/shop25-12.sql
@@ -19,8 +19,6 @@ SET time_zone = "+00:00";
 --
 -- Database: `shop`
 --
-CREATE DATABASE IF NOT EXISTS `shop` DEFAULT CHARACTER SET utf8 COLLATE utf8_general_ci;
-USE `shop`;
 
 -- --------------------------------------------------------
 
